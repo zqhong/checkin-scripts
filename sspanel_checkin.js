e6da1cb3a9b5695bdfb398877e87dc9bfc53cd7c
sudojia <i@imzjw.cn>
2021-09-27 16:40:04 +0800

    Update

--- sspanel_checkin.js
+++ sspanel_checkin.js
@@ -1,41 +1,36 @@
 /**
- * SSPANEL面板签到
- *
  * @author Telegram@sudojia
  * @site https://blog.imzjw.cn
+ * @date 2021/9/25 22:11
+ * @last Modified by sudojia
+ * @last Modified time 2021/9/27 15:22
+ * @description SSPANEL面板自动签到
  */
 const $ = new require('./env').Env('SSPANEL面板自动签到');
 const notify = $.isNode() ? require('./sendNotify') : '';
-let accounts = $.isNode() ? (process.env.ACCOUNTS ? process.env.ACCOUNTS : '') : ($.getdata('ACCOUNTS') ? $.getdata('ACCOUNTS') : ''),
-    apis = $.isNode() ? (process.env.SITE_URL ? process.env.SITE_URL : '') : ($.getdata('SITE_URL') ? $.getdata('SITE_URL') : ''),
-    accountList = [],
-    apiList = [], message = '';
+let total = $.isNode() ? (process.env.SITE_ACCOUNTS ? process.env.SITE_ACCOUNTS : '') : ($.getdata('SITE_ACCOUNTS') ? $.getdata('SITE_ACCOUNTS') : ''),
+    totalList = [], message = '';
 
 // 如果大于 -1 说明是多账号，存入到数组中，否则就是单账号！
-if (accounts.indexOf('&') > -1) {
-    accountList = accounts.split('&');
+if (total.indexOf('&') > -1) {
+    totalList = total.split('&');
 } else {
-    accountList = [accounts];
-}
-if (apis.indexOf('&') > -1) {
-    apiList = apis.split('&');
-} else {
-    apiList = [apis];
+    totalList = [total];
 }
 
 !(async () => {
-    if (!accounts) {
+    if (!total) {
         console.log('请设置环境变量')
         return;
     }
-    for (let i = 0; i < accountList.length; i++) {
+    for (let i = 0; i < totalList.length; i++) {
         $.index = i + 1;
-        // 邮箱
-        $.email = accountList[i].trim().split(',')[0];
-        // 密码
-        $.pwd = accountList[i].trim().split(',')[1];
         // 网站
-        $.SSPANEL_API = apiList[i].trim().split('&')[0];
+        $.SITE_URL = totalList[i].split(',')[0];
+        // 邮箱
+        $.email = totalList[i].split(',')[1].split(':')[0];
+        // 密码
+        $.pwd = totalList[i].split(',')[1].split(':')[1];
         console.log(`\n*****开始第【${$.index}】个网站****\n`);
         await main();
         await $.wait(2000)
@@ -70,7 +65,7 @@ function sendMsg() {
  * @returns {*}
  */
 function checkin() {
-    $.log('开始进行签到...');
+    console.log('开始进行签到...\n');
     return new Promise((resolve) => {
         $.post(sendPost('user/checkin', ''), (err, response, data) => {
             try {
@@ -78,17 +73,17 @@ function checkin() {
                     $.log(`${JSON.stringify(err)}\n签到 API 请求失败，请检查网路重试`)
                 } else {
                     data = JSON.parse(data);
-                    message += `第【${$.index}】个网站\n`;
+                    message += `开始第【${$.index}】个网站\n`;
                     if (data.ret === 1) {
                         if (data.trafficInfo) {
-                            $.log(`${data.msg}\n今日已用：${data.trafficInfo.todayUsedTraffic}\n过去已用：${data.trafficInfo.lastUsedTraffic}\n剩余流量：${data.trafficInfo.unUsedTraffic}\n\n`);
+                            console.log(`${data.msg}\n今日已用：${data.trafficInfo.todayUsedTraffic}\n过去已用：${data.trafficInfo.lastUsedTraffic}\n剩余流量：${data.trafficInfo.unUsedTraffic}\n\n`);
                             message += `${data.msg}\n今日已用：${data.trafficInfo.todayUsedTraffic}\n过去已用：${data.trafficInfo.lastUsedTraffic}\n剩余流量：${data.trafficInfo.unUsedTraffic}\n\n`;
                         } else {
-                            $.log(`${data.msg}\n`)
+                            console.log(`${data.msg}\n`)
                             message += `${data.msg}\n\n`;
                         }
                     } else {
-                        $.log(data.msg, '\n');
+                        console.log(data.msg, '\n');
                         message += data.msg + '\n\n';
                     }
                 }
@@ -111,10 +106,10 @@ function login() {
         $.post(sendPost('auth/login', `email=${$.email}&passwd=${$.pwd}&code=`), (err, response, data) => {
             try {
                 if (err) {
-                    $.log(`登录 API 请求失败，请检查网路重试\n${JSON.stringify(err)}`)
+                    console.log(`登录 API 请求失败，请检查网路重试\n${JSON.stringify(err)}`)
                 } else {
                     data = JSON.parse(data);
-                    $.log(data.msg);
+                    console.log(data.msg, '\n');
                 }
             } catch (e) {
                 $.logErr(e, response);
@@ -127,14 +122,14 @@ function login() {
 
 function sendPost(path, body = {}) {
     return {
-        url: `${$.SSPANEL_API}/${path}`,
+        url: `${$.SITE_URL}/${path}`,
         body: body,
         headers: {
             "Accept": " application/json, text/javascript, */*; q=0.01",
             "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
             "X-requested-with": "XMLHttpRequest",
-            "Origin": `${$.SSPANEL_API}`,
-            "Referer": `${$.SSPANEL_API}`,
+            "Origin": `${$.SITE_URL}`,
+            "Referer": `${$.SITE_URL}`,
             "Accept-encoding": "gzip, deflate, br",
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36"
         }
